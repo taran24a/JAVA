42dc4118eb401a2941a746f2c2972abd7c8e5e2e
Taranjit Kaur <taranjitkaur225@gmail.com>
2025-09-08 20:38:06 +0530

    new

---
+++ DAY2/PG4.java
@@ -0,0 +1,18 @@
+// WAP for sum of digits of a number
+package DAY2;
+import java.util.*;
+public class PG4{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int n = sc.nextInt();
+        int sum = 0;
+        while(n > 0) {
+            sum += n % 10;
+            n /= 10;
+        }
+        System.out.println("Sum of digits: " + sum);
+        sc.close();
+    }
+    
+}
