d60d141a09eba11f8d007c1c65eaee2d73d08e7d
Taranjit Kaur <taranjitkaur225@gmail.com>
2025-09-09 20:20:30 +0530

    new

--- DAY3/PG2.java
+++ DAY3/PG2.java
@@ -1,10 +1,16 @@
 package DAY3;
+import java.util.*;
 public class PG2 {
     public static int add(int a, int b){
         return a + b;
     }
     public static void main(String[] args) {
-        int result = add(5, 10);
+       Scanner sc = new Scanner(System.in);
+         System.out.print("Enter first number: ");
+        int num1 = sc.nextInt();
+        System.out.print("Enter second number: ");
+        int num2 = sc.nextInt();
+        int result = add(num1, num2);
         System.out.println("Sum: " + result);
     }
     
