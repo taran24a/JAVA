601c3c2900d063733ead2453cd8bbddd1004027c
Taranjit Kaur <taranjitkaur225@gmail.com>
2025-09-09 20:13:47 +0530

    new

--- DAY2/PG4.java
+++ DAY2/PG4.java
@@ -2,6 +2,7 @@
 package DAY2;
 import java.util.*;
 public class PG4{
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
---
+++ DAY3/PG1.java
@@ -0,0 +1,13 @@
+package DAY3;
+import java.util.*;
+public class PG1 {
+     public static void print(){
+            System.out.println("Hello");
+        }
+        
+    public static void main(String[] args) {
+        print();
+    }
+        
+    }
+
