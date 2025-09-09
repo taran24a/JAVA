88a21a1bfc4e074799a1463d22572f4591f5a168
Taranjit Kaur <taranjitkaur225@gmail.com>
2025-09-09 20:18:03 +0530

    new

---
+++ DAY3/PG2.java
@@ -0,0 +1,11 @@
+package DAY3;
+public class PG2 {
+    public static int add(int a, int b){
+        return a + b;
+    }
+    public static void main(String[] args) {
+        int result = add(5, 10);
+        System.out.println("Sum: " + result);
+    }
+    
+}
