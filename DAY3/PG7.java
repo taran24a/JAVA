7132473672fc6e4b5d8853e21bcd7808c4e9c612
Taranjit Kaur <taranjitkaur225@gmail.com>
2025-09-10 20:45:41 +0530

    mew

---
+++ DAY3/PG7.java
@@ -0,0 +1,21 @@
+// WAP recursive for fibonacci series using user input
+package DAY3;
+import java.util.*;
+public class PG7 {
+    static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the number of terms: ");
+        int terms = sc.nextInt();
+        System.out.println("Fibonacci Series:");
+        for (int i = 0; i < terms; i++) {
+            System.out.print(fibonacci(i) + " ");
+        }
+    }
+}
