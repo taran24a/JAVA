d62aaa9789ca6cd3dc2f87033f8484d568063d56
Taranjit Kaur <taranjitkaur225@gmail.com>
2025-09-18 20:52:12 +0530

    new

--- DAY5/PG4.java
+++ DAY5/PG4.java
@@ -9,6 +9,7 @@ public class PG4 {
             for (int j = i + 1; j < n; j++)
                 if (arr[j] < arr[min_idx])
                     min_idx = j;
+                    
 
             int temp = arr[min_idx];
             arr[min_idx] = arr[i];
