a7691444f5b7bbc7c5a8b0fdc599cf44d9b9010e
Taranjit Kaur <taranjitkaur225@gmail.com>
2025-10-06 20:59:06 +0530

    new

--- DAY7/Car.java
+++ DAY7/Car.java
@@ -1,14 +1,31 @@
-
 public class Car {
-    String brand_Name = "Toyota";
-    String model_Name = "Fortuner";
-    int price = 5000000;
+    // String brand_Name = "Toyota";
+    // String model_Name = "Fortuner";
+    // int price = 5000000;
+
+    String brand_Name;
+    String model_Name;
+    int price;
+
+    Car(String  b , String m, int p){
+        brand_Name = b;
+        model_Name = m;
+        price = p;
+    }
+
+    
+
+    void show(){
+        System.out.println(brand_Name);
+        System.out.println(model_Name);
+        System.out.println(price);
+
+    }
 
     public static void main(String args[]) {
-        Car obj = new Car();
-        System.out.println("Brand Name: " + obj.brand_Name);
-        System.out.println("Model Name: " + obj.model_Name);
-        System.out.println("Price: " + obj.price);
+        Car c1 = new Car("Toyota", "Fortuner", 5000000);
+        c1.show();
+
     }
     
 }
