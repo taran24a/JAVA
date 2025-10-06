f01f474ca04e2095bdb3fbae9d0d0dbbcc6e4d82
Taranjit Kaur <taranjitkaur225@gmail.com>
2025-10-06 20:29:59 +0530

    new

---
+++ DAY7/Car.java
@@ -0,0 +1,14 @@
+
+public class Car {
+    String brand_Name = "Toyota";
+    String model_Name = "Fortuner";
+    int price = 5000000;
+
+    public static void main(String args[]) {
+        Car obj = new Car();
+        System.out.println("Brand Name: " + obj.brand_Name);
+        System.out.println("Model Name: " + obj.model_Name);
+        System.out.println("Price: " + obj.price);
+    }
+    
+}
