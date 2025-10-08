d82c0f50a080c5b316109fdafa09b9bbe8d1e498
Taranjit Kaur <taranjitkaur225@gmail.com>
2025-10-08 20:46:29 +0530

    new

--- DAY8/LinkedList.java
+++ DAY8/LinkedList.java
@@ -1,35 +1,58 @@
 package DAY8;
 
+//Functions of linked list:
+
+//Insert at head
+//Insert at tail
+//Insert at specific position
+//Print the list
+//Length of list
+//Delete at head
+//Delete at tail
+//Delete at specific position
+
+import java.util.*;
 class Node{
     int data;
     Node next;
+    //constructor
     Node(int data){
-        this.data = data;
-        this.next = null;
+        this.data=data;
+        this.next=null;
     }
 }
-public class LinkedList {
-    Node head=null;
-    Node tail=null;
-    public void insertAtHead(int data) {
+public class LinkedList{
+    Node head = null;
+    Node tail = null;
+    //Insert at head
+    void insertAtHead(int data){
         Node newNode = new Node(data);
-        if (head == null) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            newNode.next = head;
-            head = newNode;
+        if(head==null){
+            head=tail=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    //printing the list
+    void print(){
+        if(head==null){
+            System.out.println("List is empty");
+            return;
+        }
+        Node temp = head;
+        while(temp!=null){
+            System.out.print(temp.data+"->");
+            temp=temp.next;
         }
     }
-    public void insertAtTail(int data) {
-        Node newNode = new Node(data);
-        if (tail == null) {
-            head = newNode;
-            tail = newNode;
-        } else {
-            tail.next = newNode;
-            tail = newNode;
-            
-        }
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.insertAtHead(10);
+        list.insertAtHead(20);
+        list.insertAtHead(30);
+        list.insertAtHead(40);
+        list.insertAtHead(50);
+        list.print();
     }
 }
